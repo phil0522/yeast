eae2f3aec40d1593caf43e18f83bc96d13221500
Kevin Xiong <phonica@gmail.com>
2020-04-27 10:52:50 -0700

    Add Overlays

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { RouterModule } from '@angular/router';
 import { TagsComponent } from './tags/tags.component';
+import { WidgetsModule } from '../widgets/widgets.module';
 
 @NgModule({
   declarations: [DashboardComponent, TagsComponent],
   imports: [
     CommonModule,
     RouterModule,
+    WidgetsModule,
   ],
   exports: [
     DashboardComponent,
